09bd40c49cdc989c716eda416367ae1fc1eb780b
unknown <siaki24@gmail.com>
2021-12-20 21:57:52 -0800

    he

--- index.js
+++ index.js
@@ -98,9 +98,30 @@ function slideRight(){
 ************************/
 function countSlides(){
 	$('#counter').html(pos+1 + ' / ' + totalSlides);
-}
+};
 
 function pagination(){
 	$('#pagination-wrap ul li').removeClass('active');
 	$('#pagination-wrap ul li:eq('+pos+')').addClass('active');
+};
+
+// TOP OF PAGE BUTTON
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
